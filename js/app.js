1919e0bff46413fca9df0d5fb9810b577b6203ff
Tammy Do <tammytdo11@gmail.com>
2019-02-10 23:15:27 -0800

    added a do while loop to Q1. Troubleshooting how to loop through array and access an item

--- js/app.js
+++ js/app.js
@@ -5,14 +5,26 @@ alert('Enter Yes or No');
 
 
 // Question 1 about user's curiosity
-var curiosity = prompt('Do you have a strong sense of curiosity?').toUpperCase();
-if (curiosity === 'N' || curiosity === 'NO') {
-  alert('Hmm...');
-} else if (curiosity === 'Y' || curiosity === 'YES') { 
-  alert('It\'s a Nancy Drew thing');
-} else {
-  alert('Nancy never makes a mistake. No points. ');
-}
+var valid_answer = false;
+
+do {
+  var curiosity = prompt('Do you have a strong sense of curiosity?').toUpperCase();
+  if (curiosity === 'N' || curiosity === 'NO') {
+    alert('Hmm...');
+    valid_answer = true;
+    break;
+  } else if (curiosity === 'Y' || curiosity === 'YES') { 
+    alert('It\'s a Nancy Drew thing');
+    valid_answer = true;
+    break;
+  }
+
+  if (!valid_answer) {
+    alert('Invalid answer. Enter Yes or No.');
+  }
+} while (!valid_answer);
+
+
 console.log('Sense of curiosity: ' + curiosity);
 
 // Question 2 about user's attention to detail
@@ -22,18 +34,18 @@ if (detail === 'N' || detail === 'NO') {
 } else if (detail === 'Y' || detail === 'YES') { 
   alert('Sounds like you know the secret of the old clock');
 } else {
-  alert('Nancy never makes a mistake. No points. ');
+  alert('Invalid answer. Enter Yes or No.');
 }
 console.log('Can remember small details: ' + detail);
 
 // Question 3 about user's likeliness to jet off on a case
 var travel = prompt('Have you ever hopped on a plane on a whim?').toUpperCase();
 if (travel === 'N' || travel === 'NO') {
-  alert('I\ll bet');
+  alert('I\'ll bet');
 } else if (travel === 'Y' || travel === 'YES') { 
   alert('You\re hot on the trail!');
 } else {
-  alert('Nancy never makes a mistake. No points. ');
+  alert('Invalid answer. Enter Yes or No.');
 }
 console.log('Likely to jet set: ' + travel);
 
@@ -44,21 +56,21 @@ if (identity === 'N' || identity === 'NO') {
 } else if (identity === 'Y' || identity === 'YES') { 
   alert('You\'re hot on the trail!');
 } else {
-  alert('Nancy never makes a mistake. No points. ');
+  alert('Invalid answer. Enter Yes or No.');
 }
 console.log('Secret identity: ' + identity);
 
 
 // Question 5 about user's ownership of a magnifying glass
-var magnifying_glass = prompt('Do you have a favorite magnifying glass?').toUpperCase();
+var magnifying_glass = prompt('Do you have a magnifying glass?').toUpperCase();
 if (magnifying_glass === 'N' || magnifying_glass === 'NO') {
   alert('Hmm...');
 } else if (magnifying_glass === 'Y' || magnifying_glass === 'YES') { 
   alert('The ultimate deadly device');
 } else {
-  alert('Nancy never makes a mistake. No points. ');
+  alert('Invalid answer. Enter Yes or No.');
 }
-console.log('Favorite magnifying glass: ' + magnifying_glass);
+console.log('Has a magnifying glass: ' + magnifying_glass);
 
 // Repeat back responses
 alert('You answered: ' + curiosity + ', ' + detail + ', ' + travel + ', ' + identity + ', ' + magnifying_glass);
@@ -68,31 +80,62 @@ alert('Bonus point for guessing which book number is the featured book of the da
 
 // Question 6 about picking the Nancy Drew book number of the day
 //confirm user guess is a float
-var book_num = 9;
+var book_num = 99;
 var user_guess = prompt('Guess the book of the day: ');
 
 
 for (var guesses = 3; guesses > 0; guesses--) {
-  if (user_guess === book_num) {
+  if (parseInt(user_guess) === book_num) {
     alert('Bingo!');
     console.log('User guessed the correct number: ' + user_guess);
     break;
   } else if (user_guess > 175) {
     console.log('User guessed too high.');
     user_guess = prompt(' There are only 175 books! Guess again.')
-  } else if (user_guess > book_num && user_guess < 175) {
-    user_guess = prompt('You guessed too high. Guess again.');
+  } else if (user_guess > book_num && user_guess <= 175) {
+    user_guess = prompt('Too high. Guess again.');
     console.log('User guess too high');
   } else if (user_guess < book_num) {
-    user_guess = prompt('You guessed too low. Guess again.');
+    user_guess = prompt('Too low. Guess again.');
     console.log('You guessed too low! Guess again.');
   } else {
     user_guess = prompt("Invalid entry. Pick a number.");
   }
 }
 
-// Question 7 about
+// if (parseInt(user_guess) !== book_num) {
+//   alert('Out of guesses.');
+//   alert('The book of the day is number ' + book_num) + '.';
+// }
 
+// // Question 7 about naming one of Nancy's main phone contacts.
+// var nancy_friends = ['her Dad', 'mr. drew', 'mr drew', 'father', 'carson drew', 'frank', 'frank hardy', 'joe', 'joe hardy', 'bess', 'bess marvin', 'geroge', 'george fayne', 'hannah', 'hannah gruen', 'ned', 'ned nickerson'];
+// var user_guess_friends = prompt('Name one person Nancy can call to help solve a mystery.').toLowerCase();
+
+// for (i = 0; i < 6; i++) {
+//   if (user_guess_friends in nancy_friends) {
+//     alert('Correct!');
+//     console.log('User correctly answered '+ user_guess_friends + ' as Nancy\'s friend');
+//     break;
+//   } else (user_guess_friends !== nancy_friends[i]) {
+//     user_guess_friends = prompt('Incorrect. Guess again.');
+//     console.log('Incorrect answer: ' + user_guess_friends);
+//     continue;
+//   }
+// }
+
+// if (user_guess_friends !== nancy_friends[j]) {
+//   alert('Better go re-read the books.');
+// }
+
+  // for (var guesses2 = 6; guesses2 > 0; guesses2--) {
+  //   if (nancy_friends[user_guess_friends] === nancy_friends[j]){
+  //     alert('Correct!');
+  //     console.log('User entered '+ user_guess_friends + ' as Nancy\'s friend');
+  //   } else if (nancy_friends[user_guess_friends] !== nancy_friends[j]) {
+  //     alert('Incorrect. You have ' + guesses +' more guesses.'); 
+  //   }
+  // }
 // Tally points
 
 // Confirm if they are a Nancy Drew
